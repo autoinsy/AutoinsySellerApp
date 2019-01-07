6fff280bd86e842a009e9e5d9e340a4b950f78e5
dawei <dawei801230@hotmail.com>
2019-01-07 17:14:31 +0800

    增加品牌列表

--- app/src/main/java/com/autionsy/seller/constant/Constant.java
+++ app/src/main/java/com/autionsy/seller/constant/Constant.java
@@ -3,4 +3,6 @@ package com.autionsy.seller.constant;
 public class Constant {
     public static String HTTP_URL = "http://192.168.1.204:8082/api/app/";
     public static String HOME_NEWS = "news/allNews";
+
+    public static String BRAND = "";
 }
