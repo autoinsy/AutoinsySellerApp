35225ac8850c5fe0151cc309068bcb7c03424468
dawei <dawei801230@hotmail.com>
2019-02-13 10:17:35 +0800

    增加倒计时获取验证码

--- app/src/main/java/com/autionsy/seller/activity/RegisterActivity.java
+++ app/src/main/java/com/autionsy/seller/activity/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.autionsy.seller.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,12 +23,17 @@ public class RegisterActivity extends BaseActivity {
     EditText register_password_et;
     @BindView(R.id.input_register_verify_code_et)
     EditText input_register_verify_code_et;
+    @BindView(R.id.get_verify_code_tv)
+    TextView get_verify_code_tv;
 
     private String username;
     private String password;
     private String verifyCode;
 
     private Intent intent;
+
+    //new倒计时对象,总共的时间,每隔多少秒更新一次时间
+    final MyCountDownTimer myCountDownTimer = new MyCountDownTimer(60000,1000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +47,6 @@ public class RegisterActivity extends BaseActivity {
     private void initView(){
         title_tv.setVisibility(View.VISIBLE);
         title_tv.setText(R.string.register);
-
         username = input_register_username_et.getText().toString().trim();
         password = register_password_et.getText().toString().trim();
         verifyCode = input_register_verify_code_et.getText().toString().trim();
@@ -62,8 +67,34 @@ public class RegisterActivity extends BaseActivity {
                 startActivity(intent);
                 break;
             case R.id.get_verify_code_tv:
-
+                myCountDownTimer.start();
                 break;
+        }
+    }
+
+    //倒计时函数
+    private class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        //计时过程
+        @Override
+        public void onTick(long l) {
+            //防止计时过程中重复点击
+            get_verify_code_tv.setClickable(false);
+            get_verify_code_tv.setText(l/1000+"秒后重新发送");
+
+        }
+
+        //计时完毕的方法
+        @Override
+        public void onFinish() {
+            //重新给Button设置文字
+            get_verify_code_tv.setText("重新获取验证码");
+            //设置可点击
+            get_verify_code_tv.setClickable(true);
         }
     }
 }
