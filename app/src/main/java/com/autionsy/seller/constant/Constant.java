776bda578b9bd6db1a3e8f6cc95a5645e5d32a5b
Jiang dawei <dawei801230@163.com>
2019-03-30 22:12:01 +0800

    add API

--- app/src/main/java/com/autionsy/seller/constant/Constant.java
+++ app/src/main/java/com/autionsy/seller/constant/Constant.java
@@ -1,9 +1,5 @@
 package com.autionsy.seller.constant;
 
 public class Constant {
-    public static String HTTP_URL = "http://192.168.1.204:8082/api/app/";
-    public static String HOME_NEWS = "news/allNews";
-
-    public static String BRAND = "";
-    public static String UPLOAD_HEADER_IMAGE = "";
+    public static String HTTP_URL = "http://192.168.1.204:8082/seller/api/";
 }
